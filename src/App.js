06a5b1321233681c8c1778d95097f8a2697f685a
yathansh13 <yathansh1312@gmail.com>
2023-09-03 17:53:21 +0530

    Added list

--- src/App.js
+++ src/App.js
@@ -28,15 +28,19 @@ function App() {
       <div className="banner">
         <img className="banner__image" src={Banner} alt="banner" />
         <div className="banner__text">
-          <p className="t1">Quick & Fast</p>
-          <p className="t2">HOME APPLIANCE FIXER</p>
-          <p className="t3">In Dubai</p>
-          <p className="t1">WE REPAIR ALL COMMERCIAL & HOME APPLIANCES</p>
-          <CallButton link="https://wa.me/971566060173" text="Whatsapp" />
+          <div className="banner__hide">
+            <p className="t1">Quick & Fast</p>
+            <p className="t2">HOME APPLIANCE FIXER</p>
+            <p className="t3">In Dubai</p>
+            <p className="t1">WE REPAIR ALL COMMERCIAL & HOME APPLIANCES</p>
+            <CallButton link="https://wa.me/971566060173" text="Whatsapp" />
+          </div>
         </div>
       </div>
       <div className="mobBanner">
         <p className="t2m">HOME APPLIANCE FIXER</p>
+
+        <CallButton link="https://wa.me/971566060173" text="Whatsapp" />
 
         <hr />
       </div>
@@ -75,6 +79,11 @@ function App() {
       <h1 className="t4">Our Services</h1>
       <div className="services">
         <Service
+          image={washer}
+          title="Washer Dryer Repair"
+          description="Our washing machine repair services in Dubai cover motor problems, drainage issues, and malfunctioning parts. Contact us now for affordable and reliable services."
+        />
+        <Service
           image={fridge2}
           title="Refrigerator Repair"
           description="Our refrigerator repair services in Dubai cover fixing leaks, replacing compressors, and repairing faulty thermostats. Contact us now for fast and affordable services that ensure optimal cooling efficiency."
@@ -85,11 +94,6 @@ function App() {
           description="Our dishwasher repair services in Dubai cover motor problems, drainage issues, and malfunctioning parts. Contact us now for quick and reliable services that ensure optimal dishwasher performance."
         />
 
-        <Service
-          image={washer}
-          title="Washer Dryer Repair"
-          description="Our washing machine repair services in Dubai cover motor problems, drainage issues, and malfunctioning parts. Contact us now for affordable and reliable services."
-        />
         <Service
           image={oven}
           title="Cooking Range Repair"
@@ -110,6 +114,65 @@ function App() {
         <Company image={indesit} />
       </div>
       <hr />
+      <h1 className="t4">Locations we serve</h1>
+      <div className="location">
+        <div className="location__1">
+          <ul>
+            <li className="location__list">Al-Qusais</li>
+            <li className="location__list">Muhaisnah</li>
+            <li className="location__list">Al- Mizhar</li>
+            <li className="location__list">Mirdif</li>
+            <li className="location__list">Jumeirah Beach</li>
+            <li className="location__list">Jumeirah Island</li>
+            <li className="location__list">Jumeirah Lakes Tower</li>
+            <li className="location__list">Dubai Production City</li>
+            <li className="location__list">Jumeirah Golf Estates Club House</li>
+            <li className="location__list">Dubai Sports</li>
+          </ul>
+          <div className="location__1">
+            <ul>
+              <li className="location__list">Dubai Marina</li>
+              <li className="location__list">Sheikh Zayed road</li>
+              <li className="location__list">Media City</li>
+              <li className="location__list">Barsha Heights</li>
+              <li className="location__list">Dubai Internet City</li>
+              <li className="location__list">The Greens</li>
+              <li className="location__list">Emirates Living</li>
+              <li className="location__list">The Palm Jumeirah</li>
+              <li className="location__list">The Gardens</li>
+              <li className="location__list">Jumeirah Park</li>
+            </ul>
+          </div>
+          <div className="location__1">
+            <ul>
+              <li className="location__list">Arabian Ranches</li>
+              <li className="location__list">Dubai Investment Park</li>
+              <li className="location__list">Falcon City of Wonders</li>
+              <li className="location__list">The Villa </li>
+              <li className="location__list">Dubai Land</li>
+              <li className="location__list">Dubai Silicon Oasis</li>
+              <li className="location__list">Discovery Gardens</li>
+              <li className="location__list">International City</li>
+              <li className="location__list">Um Suqeim</li>
+              <li className="location__list">Al Quoz</li>
+            </ul>
+          </div>
+          <div className="location__1">
+            <ul>
+              <li className="location__list">Al Safa</li>
+              <li className="location__list">Business Bay</li>
+              <li className="location__list">Downtown Dubai</li>
+              <li className="location__list">Jumeirah</li>
+              <li className="location__list">DIFC</li>
+              <li className="location__list">Al Satwa</li>
+              <li className="location__list">Bur Dubai</li>
+              <li className="location__list">Oud Mehta</li>
+              <li className="location__list">Al Karama</li>
+              <li className="location__list">Dubai Water Front</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
